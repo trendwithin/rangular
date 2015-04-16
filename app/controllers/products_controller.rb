b6930601d334fab2013991b517a2885ab0d70592
Michael Becco <e4e6d4d5@gmail.com>
2015-04-16 08:34:19 -0700

    Delete Functions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,7 +25,7 @@ class ProductsController < ApplicationController
 
   def update
     if product.update(product_params)
-      render json: product, status 201, location: product
+      render json: product, status: 201, location: product
     else
       render json: product.errors, status: 422
     end
